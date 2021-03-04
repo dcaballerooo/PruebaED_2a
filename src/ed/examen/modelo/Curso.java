7d59c6370a34e6ede9ec9195494bfa41b28cbdc2
dcaballerooo <daw003.2021@gmail.com>
2021-03-04 02:00:42 +0100

    Documentada <Curso>

--- src/ed/examen/modelo/Curso.java
+++ src/ed/examen/modelo/Curso.java
@@ -3,13 +3,20 @@ package ed.examen.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+/**Clase que gestiona un Curso con alumnos 
+ * @author Daniel
+ * @version 2.0.0
+ *
+ */
+
 public class Curso {
 	
 	private List<Persona> listaAlumnos;
 
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**Metodo para eliminar un Alumno; su dni debe ser valido
+	 * @param dni parametro del dni del alumno a eliminar
+	 * @throws Exception Excepción lanzada si el dni no tiene la lonigtud adecuada
+	 */
 	public void eliminarAlumno(String dni) throws Exception {
 		if(dni.length()==9) {//comprobar la longitud del dni
 			listaAlumnos.remove(new Persona(dni, "", "")); //solo hace falta el dni
@@ -18,16 +25,17 @@ public class Curso {
 		}
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**Método para aniadir un alumno; debemos tener creadas Objetos persona
+	 * @param p Objeto persona; necesario para aniadir alumno
+	 */
 	public void aniadirAlumno(Persona p) {
 		listaAlumnos.add(p);
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**Metodo poara comprobar si un Alumno ya esta registrado 
+	 * @param dni dni del alumno 
+	 * @return devuelve true or false en funcion de lo ha encontrado o no
+	 */
 	public Boolean estaRegistrado(String dni) {
 		int i =0;
 		Boolean encontrado=false;
@@ -40,16 +48,16 @@ public class Curso {
 		return encontrado;
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**Metodo para la creacion de un nuevo curso mediante una lista
+	 * 
+	 */
 	public Curso() {
 		listaAlumnos= new ArrayList<Persona>();
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**Metodo para saber el numero de alumnos de un curso
+	 * @return devuelve el numero de alumnos que hay en el curso
+	 */
 	public Integer numeroAlumnos() {return listaAlumnos.size();}
 	
 	
