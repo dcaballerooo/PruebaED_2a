c9653a946e317faeb424b98c5febc53efc67ffe5
dcaballerooo <daw003.2021@gmail.com>
2021-03-04 01:22:28 +0100

    implementacion test <EliminarAlumno>

---
+++ src/ed/examen/modelo/CursoTest.java
@@ -0,0 +1,43 @@
+package ed.examen.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+	private Curso c1= new Curso();
+	private Persona p1 = new Persona("18185158R","Daniel","Caballero");
+
+	@Test
+	void testEliminarAlumno() throws Exception {
+		c1.aniadirAlumno(p1);
+
+		Integer Numerodealumnosantes = c1.numeroAlumnos();
+
+		c1.eliminarAlumno(p1.getDni());
+
+		assertEquals(Numerodealumnosantes.intValue() - 1, c1.numeroAlumnos().intValue());
+		assertFalse(c1.estaRegistrado(p1.getDni()));
+	}
+
+	@Test
+	void testAniadirAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaRegistrado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNumeroAlumnos() {
+		fail("Not yet implemented");
+	}
+
+}
